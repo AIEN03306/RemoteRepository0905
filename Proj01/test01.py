4801774a58e0463b190c093bdac5cbdeb3dda992
Warner <warnerchen1994@gmail.com>
2018-09-05 15:47:33 +0800

    comment 088

--- Proj01/test01.py
+++ Proj01/test01.py
@@ -1,10 +1,8 @@
 print('aien0322 was here')
 print('heheheh')
 print('heheheh')
-<<<<<<< HEAD
 print('foo bar')
-=======
-<<<<<<< HEAD
+
 import time
 name = input("請輸入姓名:")
 print("Hello", name, sep='-', end=',')
@@ -15,7 +13,6 @@ print("NOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO~~~~~~~~~~~")
 time.sleep(2)
 print("I'm so sorry", name, ", it's just a joke.")
 time.sleep(3)
-=======
+
 print('I am No.1')
->>>>>>> 11669d33a13ab4bef564cef46c53a7fc1f63be95
->>>>>>> 7ee51e74cbc235692c176701bfe2a78c6d3dc009
+
