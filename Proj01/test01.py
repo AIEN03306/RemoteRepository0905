7d3793f255916cf4260b28172b9a4cef4e687909
Remy <remy1124d@gmail.com>
2018-09-05 15:47:15 +0800

    commit 20

--- Proj01/test01.py
+++ Proj01/test01.py
@@ -1,7 +1,24 @@
-print('aien0322 was here')
-print('heheheh')
-print('heheheh')
-<<<<<<< HEAD
-=======
-print('I am No.1')
->>>>>>> 11669d33a13ab4bef564cef46c53a7fc1f63be95
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+123123123
