55257d67719ae9f7ffd189227906a3c4a2e4a235
Alan <Alan@gmail.com>
2018-09-05 15:46:40 +0800

    com0346

--- Proj01/test01.py
+++ Proj01/test01.py
@@ -1,8 +1,6 @@
 print('aien0322 was here')
 print('heheheh')
 print('heheheh')
-<<<<<<< HEAD
-import time
 name = input("請輸入姓名:")
 print("Hello", name, sep='-', end=',')
 time.sleep(0.5)
@@ -12,6 +10,5 @@ print("NOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO~~~~~~~~~~~")
 time.sleep(2)
 print("I'm so sorry", name, ", it's just a joke.")
 time.sleep(3)
-=======
 print('I am No.1')
->>>>>>> 11669d33a13ab4bef564cef46c53a7fc1f63be95
+
--- Proj01/test03.py
+++ Proj01/test03.py
@@ -1,11 +1,8 @@
 print('hahaha')
 print('hahaha')
-<<<<<<< HEAD
 print('aien0320 makes a change')
 print('hah')
 print('aien0322 was here')
-=======
-<<<<<<< HEAD
 print('I am No.1')
 print('hah')
 print('aien0322 was here')
