6421b053fe1a5af8ffe46279bd458a50e327e59e
Aaron <aien0322@gmail.com>
2018-09-05 15:53:54 +0800

    random

--- Proj01/test01.py
+++ Proj01/test01.py
@@ -1,7 +1,7 @@
 print('aien0322 was here')
 print('heheheh')
 print('heheheh')
-<<<<<<< HEAD
+
 import time
 name = input("請輸入姓名:")
 print("Hello", name, sep='-', end=',')
@@ -12,6 +12,5 @@ print("NOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO~~~~~~~~~~~")
 time.sleep(2)
 print("I'm so sorry", name, ", it's just a joke.")
 time.sleep(3)
-=======
+
 print('I am No.1')
->>>>>>> 11669d33a13ab4bef564cef46c53a7fc1f63be95
